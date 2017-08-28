942bc5d442d32060b56995ab68d821a8e3c67c5d
OuyangChao <ouyangchao16@gmail.com>
2017-08-28 15:34:40 +0800

    7. Fibonacci

--- 7. 斐波那契数列.cpp
+++ 7. 斐波那契数列.cpp
@@ -4,12 +4,21 @@ n<=39
 */
 
 /*
-
+思路：自底向上
 */
 
 class Solution {
 public:
     int Fibonacci(int n) {
-
+		if (n == 0 || n == 1)
+			return n;
+		vector<int> ivec(n + 1, 0);
+		ivec[0] = 0;
+		ivec[1] = 1;
+		for (size_t i = 2; i < n + 1; ++i)
+		{
+			ivec[i] = ivec[i - 1] + ivec[i - 2];
+		}
+		return ivec[n];
     }
 };
